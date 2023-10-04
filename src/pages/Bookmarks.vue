192b876a54ccc20ec0aca17ca4160907ad5bc699
mhtror669 <110927823+mhtror669@users.noreply.github.com>
2023-10-04 15:13:35 +0330

    Add files via upload

---
+++ src/pages/Bookmarks.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {VApp, VAppBar, VAppBarTitle, VAppBarNavIcon, VContainer, VRow, VCol} from "vuetify/components";
+import MovieCard from "@/components/MovieCard.vue";
+import router from "@/router";
+
+const bookmarks = localStorage.bookmarks !== undefined ? JSON.parse(localStorage.bookmarks).reverse() : []
+
+</script>
+
+<template>
+    <v-app>
+        <v-app-bar color="blue-darken-2">
+            <v-app-bar-nav-icon @click.stop="router.back()" variant="text"><v-icon>mdi-arrow-right</v-icon></v-app-bar-nav-icon>
+            <v-app-bar-title>نشان ها</v-app-bar-title>
+        </v-app-bar>
+        <p class="msg" v-if="bookmarks.length < 1">هیچ موردی نشان نشده است</p>
+        <v-container v-else class="mt-14">
+            <v-row>
+                <v-col cols="6" sm="4" md="3" lg="2" v-for="movie of bookmarks">
+                    <movie-card :movie="movie"></movie-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
+</template>
+
+<style scoped>
+.msg {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ src/pages/Search.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import {VApp, VList, VListItem, VIcon} from "vuetify/components";
+    import SearchBar from "@/components/SearchBar.vue";
+    import router from "@/router";
+
+    let searchHistory = localStorage.search === undefined ? [] : JSON.parse(localStorage.search).reverse()
+</script>
+
+<template>
+    <v-app>
+        <search-bar :focus="true"></search-bar>
+        <v-list class="mt-14 pt-4">
+            <v-list-item v-for="searchText in searchHistory" prepend-icon="mdi-history" @click="router.push(`/search/${encodeURIComponent(searchText)}`)">{{ searchText }}</v-list-item>
+        </v-list>
+    </v-app>
+</template>
+
+<style scoped></style>
